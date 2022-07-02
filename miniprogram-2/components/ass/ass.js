232e837f93a6184bd27d0e6629025e75f649df72
fei1018 <1146959611@qq.com>
2022-07-02 10:36:32 +0800

    修改单词拼写错误

--- miniprogram-2/components/ass/ass.js
+++ miniprogram-2/components/ass/ass.js
@@ -1,30 +1,30 @@
 // components/ass.js
 Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-    title:{
-      typee:String,
-      value:""
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        title: {
+            type: String,
+            value: ""
+        },
+        num: {
+            type: Number,
+            value: ""
+        }
     },
-    num:{
-      type:Number,
-      value:""
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
     }
-  },
-
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  }
 })
